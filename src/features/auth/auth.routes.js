69e8e8ceeb6cc659fa755f82cacbf80812e0b5a7
Muhammed <muhammedrefaat2020@gmail.com>
2025-02-19 22:21:44 +0200

    refactor: made logout get request in routes!e

--- src/features/auth/auth.routes.js
+++ src/features/auth/auth.routes.js
@@ -5,7 +5,7 @@ const { authorize } = require("../../shared/middleware/auth.middleware");
 
 authRouter.post("/login", authController.login);
 authRouter.post("/register", authController.register);
-authRouter.post("/logout", authorize, authController.logout);
+authRouter.get("/logout", authorize, authController.logout);
 authRouter.get("/isAuthenticated", authorize,authController.isAuthenticated);
 
 module.exports = { authRouter };
